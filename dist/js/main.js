a71e577921094a662ce30b18b0049817427cb30e
happilyafter <happilyafter.invitation@gmail.com>
2021-04-05 03:40:32 +0700

    commit kedua

--- dist/js/main.js
+++ dist/js/main.js
@@ -11,7 +11,20 @@ if ($('.countdown').length) {
     });
 }
 
+//get url
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+    }
 
+    var sn = getParameterByName('to');
+
+    document.getElementById("write").innerHTML = sn;
 
 
 })(jQuery);
